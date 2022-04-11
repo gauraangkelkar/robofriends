beabde47cedf7bc62159397c1fb8a67289985b12
Gauraang Kelkar <gauraangkelkar@gmail.com>
2022-04-10 23:18:21 -0400

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,17 @@
+export const robots = [
+    {
+        id:1,
+        name:'Gauraang',
+        email: 'gk@gmail.com'
+    },
+    {
+        id:2,
+        name:'Shruti',
+        email: 'sk@gmail.com'
+    },
+    {
+            id:3,
+            name:'Siddhesh',
+            email: 'ss@gmail.com'
+    }
+];
